9191b75c8c9c9b3a312005c8d1815adeac023f31
WojcikMikolaj <52234302+wojcikmikolaj@users.noreply.github.com>
2022-04-25 15:19:01 +0200

    dodanie podstaw pod commandy

---
+++ MikCAD/MainWindow/EditMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using MikCAD.Annotations;
+using MikCAD.BezierCurves;
+using MikCAD.Utilities;
+
+namespace MikCAD;
+
+public partial class MainWindow
+{
+    private void OnSaveCommand(object sender, RoutedEventArgs e)
+    {
+        FileDialog diag = new SaveFileDialog();
+        if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            
+        }
+    }
+    
+    private void OnLoadCommand(object sender, RoutedEventArgs e)
+    {
+        FileDialog diag = new OpenFileDialog();
+        if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            
+        }
+    }
+}
--- MikCAD/Shaders/InterpolatingBezierCurveC2/InterpolatingBezierCurveC2TessControlShader.tesc
+++ MikCAD/Shaders/InterpolatingBezierCurveC2/InterpolatingBezierCurveC2TessControlShader.tesc
@@ -6,10 +6,6 @@ uniform int tessLevels;
 void main()
 {
     gl_TessLevelOuter[0] = 1;
-    gl_TessLevelOuter[1] = tessLevels;
-    if(gl_InvocationID%4==0)
-    {
-        gl_TessLevelOuter[1] = max(64, 1024 * int(gl_in[gl_InvocationID].gl_Position.w));
-    }
+    gl_TessLevelOuter[1] = int(gl_InvocationID%4!=0) * tessLevels + int(gl_InvocationID%4==0) * max(64, 1024 * int(gl_in[gl_InvocationID].gl_Position.w));
     gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 }

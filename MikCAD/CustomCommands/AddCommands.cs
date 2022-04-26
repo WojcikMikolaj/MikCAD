4e56bb045b550cf41efef63e2fbdee92b1b2a16a
Mikołaj Wójcik <52234302+WojcikMikolaj@users.noreply.github.com>
2022-04-26 21:45:49 +0200

    Skróty klawiszowe, Todo zaznaczenia i ich zwalnianie

---
+++ MikCAD/CustomCommands/AddCommands.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace MikCAD.CustomCommands;
+
+public static class AddCommands
+{
+    public static RoutedCommand AddPoint = new RoutedCommand("AddPointCommand", typeof(MainWindow));
+    public static RoutedCommand AddTorus = new RoutedCommand("AddTorusCommand", typeof(MainWindow));
+    public static RoutedCommand AddBezierCurveC0 = new RoutedCommand("AddBezierCurveC0", typeof(MainWindow));
+    public static RoutedCommand AddBezierCurveC2 = new RoutedCommand("AddBezierCurveC2", typeof(MainWindow));
+    public static RoutedCommand AddInterpolatingBezierCurveC2 = new RoutedCommand("AddInterpolatingBezierCurveC2", typeof(MainWindow));
+}

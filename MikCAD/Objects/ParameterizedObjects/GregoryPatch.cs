c8d0205873f244c6763e876ca35dcdc1d42ab6f6
Mikołaj Wójcik <52234302+WojcikMikolaj@users.noreply.github.com>
2022-06-10 12:20:45 +0200

    czas na rysowanie

--- MikCAD/Objects/ParameterizedObjects/GregoryPatch.cs
+++ MikCAD/Objects/ParameterizedObjects/GregoryPatch.cs
@@ -37,22 +37,22 @@ public class GregoryPatch : ParameterizedObject, I2DObject
     private List<ParameterizedPoint>[] outerBezierControlPoint;
 
     private Vector3[] B = new Vector3[3];
-    private Vector3[,] R = new Vector3[3,2];
-    private Vector3[,] S = new Vector3[3,2];
+    private Vector3[,] R = new Vector3[3, 2];
+    private Vector3[,] S = new Vector3[3, 2];
     private Vector3[] T = new Vector3[3];
-    
-    private Vector3[,] RpD = new Vector3[3,2];
-    private Vector3[,] SpD = new Vector3[3,2];
-    
+
+    private Vector3[,] RpD = new Vector3[3, 2];
+    private Vector3[,] SpD = new Vector3[3, 2];
+
     private Vector3[] P2 = new Vector3[3];
     private Vector3[] ai = new Vector3[3];
     private Vector3 p0;
     private Vector3[] P1 = new Vector3[3];
     private Vector3[,] RedPoints = new Vector3[3, 2];
-    
+
     private Vector3[] OB = new Vector3[3];
-    private Vector3[,] OR = new Vector3[3,2];
-    private Vector3[,] OS = new Vector3[3,2];
+    private Vector3[,] OR = new Vector3[3, 2];
+    private Vector3[,] OS = new Vector3[3, 2];
     private Vector3[] OT = new Vector3[3];
 
     public void ReconstructPatch()
@@ -80,39 +80,45 @@ public class GregoryPatch : ParameterizedObject, I2DObject
                 outerBezierControlPoint[i][3]._position
             };
 
-            R[i,0] = MiddlePoint(IP[0], IP[1]);
-            R[i,1] = MiddlePoint(IP[2], IP[3]);
+            R[i, 0] = MiddlePoint(IP[0], IP[1]);
+            R[i, 1] = MiddlePoint(IP[2], IP[3]);
             pom = MiddlePoint(IP[1], IP[2]);
-            S[i,0] = MiddlePoint(R[i,0], pom);
-            S[i,1] = MiddlePoint(pom, R[i,1]);
-            T[i] = MiddlePoint(S[i,0], S[i,1]);
-            
-            OR[i,0] = MiddlePoint(OP[0], OP[1]);
-            OR[i,1] = MiddlePoint(OP[2], OP[3]);
+            S[i, 0] = MiddlePoint(R[i, 0], pom);
+            S[i, 1] = MiddlePoint(pom, R[i, 1]);
+            T[i] = MiddlePoint(S[i, 0], S[i, 1]);
+
+            OR[i, 0] = MiddlePoint(OP[0], OP[1]);
+            OR[i, 1] = MiddlePoint(OP[2], OP[3]);
             pom = MiddlePoint(OP[1], OP[2]);
-            OS[i,0] = MiddlePoint(OR[i,0], pom);
-            OS[i,1] = MiddlePoint(pom, OR[i,1]);
-            OT[i] = MiddlePoint(OS[i,0], OS[i,1]);
+            OS[i, 0] = MiddlePoint(OR[i, 0], pom);
+            OS[i, 1] = MiddlePoint(pom, OR[i, 1]);
+            OT[i] = MiddlePoint(OS[i, 0], OS[i, 1]);
 
             RpD[i, 0] = R[i, 0] + (R[i, 0] - OR[i, 0]);
             RpD[i, 1] = R[i, 1] + (R[i, 1] - OR[i, 1]);
-            
+
             SpD[i, 0] = S[i, 0] + (S[i, 0] - OS[i, 0]);
             SpD[i, 1] = S[i, 1] + (S[i, 1] - OS[i, 1]);
 
             P2[i] = T[i] + (T[i] - OT[i]);
-            
-            ai[i] = Vector3.Lerp(T[i],P2[i],1.5f);
+
+            ai[i] = Vector3.Lerp(T[i], P2[i], 1.5f);
         }
 
         p0 = (ai[0] + ai[1] + ai[2]) / 3;
         for (int i = 0; i < 3; i++)
         {
-            P1[i] = Vector3.Lerp(p0,ai[i],2.0f/3);
+            P1[i] = Vector3.Lerp(p0, ai[i], 2.0f / 3);
         }
 
-        RedPoints[0, 0] = P1[0] - (SpD[0,0] - P2[0]).Normalized() / 3;
-        RedPoints[2, 1] = P1[0] + (SpD[0,0] - P2[0]).Normalized() / 3;
+        RedPoints[0, 0] = P1[0] - (SpD[0, 0] - P2[0]) / 3;
+        RedPoints[2, 1] = P1[0] + (SpD[0, 0] - P2[0]) / 3;
+
+        RedPoints[1, 0] = P1[1] - (SpD[1, 0] - P2[1]) / 3;
+        RedPoints[0, 1] = P1[1] + (SpD[1, 0] - P2[1]) / 3;
+
+        RedPoints[2, 0] = P1[2] - (SpD[2, 0] - P2[2]) / 3;
+        RedPoints[1, 1] = P1[2] + (SpD[2, 0] - P2[2]) / 3;
 
         for (int i = 0; i < 3; i++)
         {
@@ -124,27 +130,27 @@ public class GregoryPatch : ParameterizedObject, I2DObject
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"R{i},0")
             {
-                posX = R[i,0].X,
-                posY = R[i,0].Y,
-                posZ = R[i,0].Z
+                posX = R[i, 0].X,
+                posY = R[i, 0].Y,
+                posZ = R[i, 0].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"R{i},1")
             {
-                posX = R[i,1].X,
-                posY = R[i,1].Y,
-                posZ = R[i,1].Z
+                posX = R[i, 1].X,
+                posY = R[i, 1].Y,
+                posZ = R[i, 1].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"S{i},0")
             {
-                posX = S[i,0].X,
-                posY = S[i,0].Y,
-                posZ = S[i,0].Z
+                posX = S[i, 0].X,
+                posY = S[i, 0].Y,
+                posZ = S[i, 0].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"R{i},1")
             {
-                posX = S[i,1].X,
-                posY = S[i,1].Y,
-                posZ = S[i,1].Z
+                posX = S[i, 1].X,
+                posY = S[i, 1].Y,
+                posZ = S[i, 1].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"T{i}")
             {
@@ -152,39 +158,39 @@ public class GregoryPatch : ParameterizedObject, I2DObject
                 posY = T[i].Y,
                 posZ = T[i].Z,
             });
-            
+
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"RpD{i},0")
             {
-                posX = RpD[i,0].X,
-                posY = RpD[i,0].Y,
-                posZ = RpD[i,0].Z
+                posX = RpD[i, 0].X,
+                posY = RpD[i, 0].Y,
+                posZ = RpD[i, 0].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"RpD{i},1")
             {
-                posX = RpD[i,1].X,
-                posY = RpD[i,1].Y,
-                posZ = RpD[i,1].Z
+                posX = RpD[i, 1].X,
+                posY = RpD[i, 1].Y,
+                posZ = RpD[i, 1].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"SpD{i},0")
             {
-                posX = SpD[i,0].X,
-                posY = SpD[i,0].Y,
-                posZ = SpD[i,0].Z
+                posX = SpD[i, 0].X,
+                posY = SpD[i, 0].Y,
+                posZ = SpD[i, 0].Z
             });
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"SpD{i},1")
             {
-                posX = SpD[i,1].X,
-                posY = SpD[i,1].Y,
-                posZ = SpD[i,1].Z
+                posX = SpD[i, 1].X,
+                posY = SpD[i, 1].Y,
+                posZ = SpD[i, 1].Z
             });
-            
+
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"P2{i}")
             {
                 posX = P2[i].X,
                 posY = P2[i].Y,
                 posZ = P2[i].Z,
             });
-            
+
             Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"P1{i}")
             {
                 posX = P1[i].X,
@@ -192,27 +198,25 @@ public class GregoryPatch : ParameterizedObject, I2DObject
                 posZ = P1[i].Z,
             });
         }
+
         Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"P0")
         {
             posX = p0.X,
             posY = p0.Y,
             posZ = p0.Z,
         });
-        Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint("RP0,0")
-        {
-            posX = RedPoints[0,0].X,
-            posY = RedPoints[0,0].Y,
-            posZ = RedPoints[0,0].Z,
-        });
-        Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint("RP2,1")
-        {
-            posX = RedPoints[2,1].X,
-            posY = RedPoints[2,1].Y,
-            posZ = RedPoints[2,1].Z,
-        });
+        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++)
+            Scene.CurrentScene.ObjectsController.AddObjectToScene(new ParameterizedPoint($"RP{i},{j}")
+            {
+                posX = RedPoints[i, j].X,
+                posY = RedPoints[i, j].Y,
+                posZ = RedPoints[i, j].Z,
+            });
     }
 
     Vector3 MiddlePoint(Vector3 A, Vector3 B) => (A + B) / 2;
+
     Vector3 EvaluateCurveAtT(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
     {
         t = 1 - t;
@@ -296,6 +300,5 @@ public class GregoryPatch : ParameterizedObject, I2DObject
     public override void PassToDrawProcessor(DrawProcessor drawProcessor, EyeEnum eye, uint vertexAttributeLocation,
         uint normalAttributeLocation)
     {
-        
     }
 }

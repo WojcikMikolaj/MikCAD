0cab6478939dd2eca4cc6f53ad05f1e50df22788
Mikołaj Wójcik <52234302+WojcikMikolaj@users.noreply.github.com>
2022-10-13 21:02:57 +0200

    Symulator GUI

---
+++ MikCAD/CustomControls/Simulator3CControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MikCAD.CustomControls;
+
+public partial class Simulator3CControl : UserControl
+{
+    public Simulator3CControl()
+    {
+        InitializeComponent();
+    }
+}

449b8ff8a03ae2d255ef6a858a7883c7c5605a0d
Mikołaj Wójcik <52234302+WojcikMikolaj@users.noreply.github.com>
2022-11-18 17:09:06 +0100

    check

--- MikCAD/Sciezki/PathsGeneratorDetailed.cs
+++ MikCAD/Sciezki/PathsGeneratorDetailed.cs
@@ -1,9 +1,10 @@
 ﻿//#define DRAW
 //#define SHOW_POINTS
-//#define RACZKA
+
+#define RACZKA
 //#define CZUBEK
 
-#define SLOIK
+//#define SLOIK
 
 
 using System.Collections.Generic;
@@ -29,6 +30,90 @@ public partial class PathsGenerator
             }
         }
 
+        var detailed = new IIntersectableDecoratorStage3(surfaces[1])
+        {
+            DistanceFromSurface = radius / CmToMm
+        };
+        var detailedR = new IIntersectableDecoratorStage3(surfaces[0])
+        {
+            DistanceFromSurface = radius / CmToMm
+        };
+
+        var detailedD = new IIntersectableDecoratorStage3(surfaces[2])
+        {
+            DistanceFromSurface = radius / CmToMm
+        };
+
+        //Przecięcie z górną częścią rączki
+        // u1 = 8.440287f; v1 = 5.3860373f;
+        // u2 = 0.28591698f; v2 = 7.5931625f;
+        var intersectUp = new Intersection(detailed, detailedR)
+        {
+            MaxPointsNumber = 100,
+            StartingPointsNumber = 10000,
+            NewtonMaxIterations = 10000
+        };
+
+        var u1 = 8.440287f;
+        var v1 = 5.3860373f;
+
+        var u2 = 0.28591698f;
+        var v2 = 7.5931625f;
+        if (intersectUp.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
+                (detailedR.GetValueAt(u2, v2), u2, v2), true))
+        {
+            intersectUp.ShowC0();
+            //intersectUp.ConvertToInterpolating();
+        }
+
+        //Przecięcie z dolną częścią rączki
+        // u1 = 8.309978f; v1 = 3.2709327f;
+        // u2 = 0.24050847f; v2 = 0.511595f;
+
+        var intersectDown = new Intersection(detailed, detailedR)
+        {
+            UseCursor = true,
+            MaxPointsNumber = 200,
+            StartingPointsNumber = 10000,
+            NewtonMaxIterations = 10000
+        };
+
+        u1 = 8.309978f;
+        v1 = 3.2709327f;
+
+        u2 = 0.24050847f;
+        v2 = 0.511595f;
+
+        if (intersectDown.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
+                (detailedR.GetValueAt(u2, v2), u2, v2), true))
+        {
+            intersectDown.ShowC0();
+            //intersectDown.ConvertToInterpolating();
+        }
+
+        //Przecięcie z dziubkiem
+        // u1 = 2.566685f; v1 = 4.3139234f;
+        // u2 = 3.96224f; v2 = 0.8268527f;
+        var intersectLeft = new Intersection(detailed, detailedD)
+        {
+            MaxPointsNumber = 10000,
+            StartingPointsNumber = 10000,
+            NewtonMaxIterations = 10000
+        };
+
+        u1 = 2.566685f;
+        v1 = 4.3139234f;
+
+        u2 = 3.96224f;
+        v2 = 0.8268527f;
+
+        if (intersectLeft.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
+                (detailedD.GetValueAt(u2, v2), u2, v2), true))
+        {
+            intersectLeft.ShowC0();
+            // intersectLeft.ConvertToInterpolating();
+        }
+
         #region raczka
 
 #if RACZKA
@@ -36,38 +121,33 @@ public partial class PathsGenerator
             var finalPoints = new List<Vector3>();
             var lines = new List<List<Vector3>>();
 
-            var detailed = new IIntersectableDecoratorStage2(surfaces[0])
-            {
-                DistanceFromSurface = radius / CmToMm
-            };
             var samplesPerParam = 100;
 
-            var u = 0.0f;
-            var v = 0.0f;
+            var u = 0f;
 
-            var dU = detailed.USize / samplesPerParam;
-            var dV = detailed.VSize / samplesPerParam;
+            var dU = detailedR.USize / samplesPerParam;
+            var dV = detailedR.VSize / samplesPerParam;
 
 
             for (int i = 0; i < samplesPerParam; i++)
             {
-                v = 0.0f;
+                var v = intersectDown.GetVOnULine(u);
+                var endv = intersectUp.GetVOnULine(u); 
                 var points = new List<Vector3>();
                 for (int j = 0; j < samplesPerParam; j++)
                 {
-                    var point = detailed.GetValueAt(u, v);
+                    var point = detailedR.GetValueAt(u, v);
                     if (point.isFinite())
                     {
                         (point.Y, point.Z) = (-point.Z, point.Y);
                         point *= CmToMm;
-                        
-                        if (point.Z >= SupportSize*CmToMm - 0.1)
+
+                        if (point.Z >= SupportSize * CmToMm - 0.1)
                         {
                             points.Add(point);
                         }
                         else
                         {
-                            
                         }
                     }
 
@@ -79,11 +159,12 @@ public partial class PathsGenerator
                 {
                     points.Reverse();
                 }
+
                 finalPoints.AddRange(points);
                 nextIter: ;
             }
 
-            SavePath(frez, radius, finalPoints);
+            SavePath(frez, radius, finalPoints, false);
         }
 #endif
 
@@ -95,90 +176,6 @@ public partial class PathsGenerator
         {
             var mainPartFinalPoints = new List<Vector3>();
             var lines = new List<List<Vector3>>();
-
-            var detailed = new IIntersectableDecoratorStage3(surfaces[1])
-            {
-                DistanceFromSurface = radius / CmToMm
-            };
-            var detailedR = new IIntersectableDecoratorStage3(surfaces[0])
-            {
-                DistanceFromSurface = radius / CmToMm
-            };
-
-            var detailedD = new IIntersectableDecoratorStage3(surfaces[2])
-            {
-                DistanceFromSurface = radius / CmToMm
-            };
-
-            //Przecięcie z górną częścią rączki
-            // u1 = 8.440287f; v1 = 5.3860373f;
-            // u2 = 0.28591698f; v2 = 7.5931625f;
-            var intersectUp = new Intersection(detailed, detailedR)
-            {
-                MaxPointsNumber = 100,
-                StartingPointsNumber = 10000,
-                NewtonMaxIterations = 10000
-            };
-
-            var u1 = 8.440287f;
-            var v1 = 5.3860373f;
-
-            var u2 = 0.28591698f;
-            var v2 = 7.5931625f;
-            if (intersectUp.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
-                    (detailedR.GetValueAt(u2, v2), u2, v2), true))
-            {
-                intersectUp.ShowC0();
-                //intersectUp.ConvertToInterpolating();
-            }
-
-            //Przecięcie z dolną częścią rączki
-            // u1 = 8.309978f; v1 = 3.2709327f;
-            // u2 = 0.24050847f; v2 = 0.511595f;
-
-            var intersectDown = new Intersection(detailed, detailedR)
-            {
-                UseCursor = true,
-                MaxPointsNumber = 200,
-                StartingPointsNumber = 10000,
-                NewtonMaxIterations = 10000
-            };
-
-            u1 = 8.309978f;
-            v1 = 3.2709327f;
-
-            u2 = 0.24050847f;
-            v2 = 0.511595f;
-
-            if (intersectDown.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
-                    (detailedR.GetValueAt(u2, v2), u2, v2), true))
-            {
-                intersectDown.ShowC0();
-                //intersectDown.ConvertToInterpolating();
-            }
-
-            //Przecięcie z dziubkiem
-            // u1 = 2.566685f; v1 = 4.3139234f;
-            // u2 = 3.96224f; v2 = 0.8268527f;
-            var intersectLeft = new Intersection(detailed, detailedD)
-            {
-                MaxPointsNumber = 10000,
-                StartingPointsNumber = 10000,
-                NewtonMaxIterations = 10000
-            };
-
-            u1 = 2.566685f;
-            v1 = 4.3139234f;
-
-            u2 = 3.96224f;
-            v2 = 0.8268527f;
-
-            if (intersectLeft.Intersect((detailed.GetValueAt(u1, v1), u1, v1),
-                    (detailedD.GetValueAt(u2, v2), u2, v2), true))
-            {
-                intersectLeft.ShowC0();
-                // intersectLeft.ConvertToInterpolating();
-            }
 
             var samplesPerParam = 110;
 
